51624b90ba5e7ba133c102404a5f106c8cd2304f
mujaddidsi <mujaddid.s.i@gmail.com>
2026-07-02 15:29:32 +0700

    fix: restrict security group to ports 80 and 8080, remove overly permissive 0-65535 and SSH rules

--- infra/main.tf
+++ infra/main.tf
@@ -60,15 +60,15 @@ resource "aws_security_group" "app" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
