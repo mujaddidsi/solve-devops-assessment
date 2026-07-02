e6190375884c76e2729edae361c0527b737a3190
mujaddidsi <mujaddid.s.i@gmail.com>
2026-07-02 15:24:24 +0700

    fix: separate ECS task and execution roles, replace AdministratorAccess with least-privilege policy

--- infra/main.tf
+++ infra/main.tf
@@ -89,8 +89,8 @@ resource "aws_ecr_repository" "app" {
 
 # --- IAM ----------------------------------------------------------------
 
-resource "aws_iam_role" "ecs_task" {
-  name = "${var.app_name}-task-role"
+resource "aws_iam_role" "ecs_execution" {
+  name = "${var.app_name}-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -102,9 +102,22 @@ resource "aws_iam_role" "ecs_task" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_admin" {
-  role       = aws_iam_role.ecs_task.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task" {
+  name = "${var.app_name}-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
 }
 
 # --- Load balancer ------------------------------------------------------
