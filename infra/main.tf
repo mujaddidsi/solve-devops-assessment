c2c98380ce0220f999cdcf4c6bd47ed21e8d5c8c
mujaddidsi <mujaddid.s.i@gmail.com>
2026-07-02 15:26:12 +0700

    fix: update ECS task definition to use separate task and execution roles

--- infra/main.tf
+++ infra/main.tf
@@ -162,7 +162,7 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
   task_role_arn            = aws_iam_role.ecs_task.arn
-  execution_role_arn       = aws_iam_role.ecs_task.arn
+  execution_role_arn       = aws_iam_role.ecs_execution.arn
 
   container_definitions = jsonencode([{
     name      = var.app_name
